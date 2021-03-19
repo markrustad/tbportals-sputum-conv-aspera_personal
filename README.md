e515422595e780b615a1f4e420eb7346beccd692
Mark Rustad <madarus@gmail.com>
2021-03-19 03:38:07 -0400

    added targets df_pos_init, plot1

---
+++ README.md
@@ -0,0 +1,1 @@
+# tbportals-sputum-conv-aspera
--- _targets.R
+++ _targets.R
@@ -18,6 +18,12 @@ list(
              iteration = "list"),
   
   # generate initial dataframe from raw data
-  tar_target(df_initial, generate_df_initial(files, data))
+  tar_target(df_init, generate_df_init(files, data)),
+  
+  # filter cond. id's that have documented POSITIVE near treatment start
+  tar_target(df_pos_init, generate_df_pos_init(df_init, t1 = 14, t2 = 0)),
+  
+  # create plot target
+  tar_target(plot1, generate_plot1(df_pos_init))
   
 )
